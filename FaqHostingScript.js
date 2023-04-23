8da158aa692878c2a666d14aa169c45c0bb87311
Pavel Khristenko <102798090+8Cofiman@users.noreply.github.com>
2023-04-23 19:38:44 +0500

    Add files via upload

---
+++ FaqHostingScript.js
@@ -0,0 +1,34 @@
+/*var acc = document.getElementsByClassName("frame-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+    Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel 
+    this.classList.toggle("active");
+
+    Toggle between hiding and showing the active panel 
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+    panel.style.display = "none";
+    } else {
+    panel.style.display = "block";
+    }
+});
+}
+
+
+
+var modal= $modal({
+    title: 'Как подключиться?',
+    content: '<p>Не вызывает сомнений, что реализация намеченных плановых заданий создаёт предпосылки качественно новых шагов для поставленных обществом и правительством задач. Равным образом консультация с широким активом позволяет выполнять важные задания по разработке поэтапного и последовательного развития общества. Разнообразный и богатый опыт повышение уровня гражданского сознания напрямую зависит от дальнейших направлений развития.</p>',
+    footerButtons:[
+        { class: 'btn btn__cancel', text: 'Отмена', handler: 'modalHandlerCancel' },
+        { class: 'btn btn__ok', text: 'ОК', handler: 'modalHandlerOk' }
+    ]
+});
+
+document.querySelector("#submit").onclick=function how(){
+    modal.Show();
+}*/
+
